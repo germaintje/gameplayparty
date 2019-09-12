c612454827cbe5f6baf0de9c9a854608db9f3359
anass0347 <50557143+anass0347@users.noreply.github.com>
2019-09-12 10:16:40 +0200

    Detail page en catalog

---
+++ view/bios_catalog.php
@@ -0,0 +1,31 @@
+<?php
+
+include "header.php";
+
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+<?php
+
+include "footer.html";
+?>
---
+++ view/bios_detail.php
@@ -0,0 +1,76 @@
+<?php
+
+include "header.php";
+
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+     <img src="../media/kinepolisDenhelder.jpg" width=500px>
+    </div>
+    <div class="col-sm">
+      <p>Hier komt de tekst over de bios.</p>
+    </div>
+  </div>
+</div>
+
+
+  <div class="row">
+    <div class="col-4"></div>
+    <div class="col-4 tijdentitle">
+      <h2>Tijden en zalen</h2>
+    </div>
+    <div class="col-4"></div>
+
+    </div>
+  </div>
+
+  <div class="row eventrow">
+    <div class="col event">
+      <h2>Title</h2>
+      <p>Information</p>
+      <h6>Tijd...</h6>
+      <button type="button" class="btn btn-info">Info</button>
+    </div>
+    <div class="col event">
+      <h2>Title</h2>
+      <p>Information</p>
+      <h6>Tijd...</h6>
+      <button type="button" class="btn btn-info">Info</button>
+    </div>
+    <div class="col event">
+      <h2>Title</h2>
+      <p>Information</p>
+      <h6>Tijd...</h6>
+      <button type="button" class="btn btn-info">Info</button>
+    </div>
+    <div class="col event">
+      <h2>Title</h2>
+      <p>Information</p>
+      <h6>Tijd...</h6>
+      <button type="button" class="btn btn-info">Info</button>
+    </div>
+    <div class="col event">
+      <h2>Title</h2>
+      <p>Information</p>
+      <h6>Tijd...</h6>
+      <button type="button" class="btn btn-info">Info</button>
+    </div>
+  </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+include "footer.html";
+?>
